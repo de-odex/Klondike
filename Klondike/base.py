972b54931779d8cc93cc10908a94e6b25ed45bfe
Justin Kyle Ramos <justinfblover@gmail.com>
2018-09-24 18:10:15 +0800

    Replace debug sanity check code with base game debug code
    Signed-off-by: Justin Kyle Ramos <justinfblover@gmail.com>

--- Klondike/base.py
+++ Klondike/base.py
@@ -1,16 +1,12 @@
 import logging
 
-from . import card, game
+from . import game
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
-    base_deck = card.CardDeck(full=True)
-
-    test = game.KlondikeDeck()
-
-    test2 = card.Card("QH")
-    # card = test.deal()
-    logger.debug(test)
-    logger.debug(len(test))
+    game = game.Game()
+    logger.debug(game.base_deck)
+    logger.debug([str(i) for i in game.decks])
+    logger.debug(game.foundations)
