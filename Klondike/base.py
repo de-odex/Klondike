b7fdaf1d48f2c8fd97beabbfe02c24cdb25b0908
Justin Kyle Ramos <justinfblover@gmail.com>
2018-10-06 09:54:06 +0800

    Replace debug code with game debug code
    Signed-off-by: Justin Kyle Ramos <justinfblover@gmail.com>

--- Klondike/base.py
+++ Klondike/base.py
@@ -7,6 +7,6 @@ logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     game = game.Game()
-    logger.debug(game.base_deck)
-    logger.debug([str(i) for i in game.decks])
-    logger.debug(game.foundations)
+
+    game.take(1, int(input("take from where: "))).put(int(input("put where: ")))
+
