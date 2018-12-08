011f1bef72d351eb331bac6f8397facf926f4fc2
Justin Kyle Ramos <justinfblover@gmail.com>
2018-12-08 15:05:40 +0800

    Add info output after every move

--- Klondike/parser.py
+++ Klondike/parser.py
@@ -125,9 +125,12 @@ class CommandParser:
             raise MoveError("not enough cards to take")
 
         self.game_obj.place(card_amount, deck_identifier[0] - 1, deck_identifier[1] - 1)
+        self.info("")
 
     def draw(self, parsed):
         self.game_obj.stock_deck.pass_card()
+
+        self.info("")
 
     def info(self, parsed):
         self.game_obj.move_info()
